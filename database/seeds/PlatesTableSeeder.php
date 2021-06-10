9351fada4c1f1fb2ed01bc7da405a6f89a0c246c
Giorgio Rindonone <giorgio.rindonone@gmail.com>
2021-06-10 17:13:28 +0200

    new: seeders maledetti completati, migrations

--- database/seeds/PlatesTableSeeder.php
+++ database/seeds/PlatesTableSeeder.php
@@ -4,6 +4,7 @@ use App\Plate;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use App\User;
 
 class PlatesTableSeeder extends Seeder
 {
@@ -14,22 +15,30 @@ class PlatesTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i <10 ; $i++) { 
-            $newPlate = new Plate();
-            $newPlate->name = $faker->words(5);
-            $newPlate->price = $faker->randomfloat(1, 10, 20);
-            $newPlate->image = $faker->ImageUrl(640, 480);
-            $newPlate->ingredients = $faker->text();
-            $newPlate->description = $faker->text();
-            $newPlate->scope = $faker->words(1);
-            $newPlate->slug = Str::slug( $newPlate->name , '-');;
-            $newPlate->visible = rand(0,1);
-            $newPlate->vegan = rand(0,1);
-            $newPlate->vegetable = rand(0,1);
-            $newPlate->gluten_free = rand(0,1);
-            $newPlate->hot = rand(0,1);
-            $newPlate->save();
+        $users = User::all();
+        foreach ($users as $user)
+        {
+            for ($i = 0; $i < 10 ; $i++) { 
+                $newPlate = new Plate();
+                $newPlate->user_id = $user->id;            
+                $newPlate->name = $faker->words(5, true);
+                $newPlate->price = $faker->randomFloat(1, 10, 20);
+                $newPlate->image = $faker->ImageUrl(640, 480);
+                $newPlate->ingredients = $faker->text();
+                $newPlate->description = $faker->text();
+                $newPlate->scope = $faker->words(1, true);
+                $newPlate->slug = Str::slug( $newPlate->name , '-');
+                $newPlate->visible = rand(0,1);
+                $newPlate->vegan = rand(0,1);
+                $newPlate->vegetarian = rand(0,1);
+                $newPlate->gluten_free = rand(0,1);
+                $newPlate->hot = rand(0,1);
+                $newPlate->save();
+            }
+    
         }
+        
+
 
     }
 }
