24ca905d614f60226b0a842f94f58cd82ccad768
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-06-11 12:30:13 +0200

    add: create section

--- app/Http/Controllers/Admin/PlateController.php
+++ app/Http/Controllers/Admin/PlateController.php
@@ -6,17 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Plate;
 use App\Genre;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PlateController extends Controller
 {
+
+    protected $validation = [
+        'name' => 'required|string',
+        'price' => 'required|numeric',
+        'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ];
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {   
+    {
         //ci importiamo tutti i piatti
         //$plates = Plate::all();
 
@@ -26,7 +35,6 @@ class PlateController extends Controller
         $plates = Plate::where('user_id', $user_id)->get();
 
         return view('admin.plates.index', compact('plates'));
-
     }
 
     /**
@@ -36,7 +44,7 @@ class PlateController extends Controller
      */
     public function create()
     {
-        
+
         $genres = Genre::all();
         return view('admin.plates.create', compact('genres'));
     }
@@ -49,7 +57,35 @@ class PlateController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validazione
+        $validation = $this->validation;
+
+        $request->validate($validation);
+
+        $data = $request->all();
+
+        // salvataggio dati booleani
+        $data['visible'] = !isset($data['visible']) ? 0 : 1;
+        $data['vegan'] = !isset($data['vegan']) ? 0 : 1;
+        $data['vegetarian'] = !isset($data['vegetarian']) ? 0 : 1;
+        $data['gluten_free'] = !isset($data['gluten_free']) ? 0 : 1;
+        $data['hot'] = !isset($data['hot']) ? 0 : 1;
+        // slug nome piatto
+        $data['slug'] = Str::slug($data['name'], '-');
+
+        // upload file image
+        if (isset($data['image'])) {
+            $data['image'] = Storage::disk('public')->put('image', $data['image']);
+        }
+
+        // richiamo lo user tramite id
+        $data['user_id'] = Auth::id();
+
+        // salvo il nuovo piatto a db
+        $newPlate = Plate::create($data);
+
+        // reinvio alla index
+        return redirect()->route('admin.plates.index');
     }
 
     /**
