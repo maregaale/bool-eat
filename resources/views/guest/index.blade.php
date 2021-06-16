af3fecf6f39e94112ac8de834048867c299c8f7f
Alessandro Marega <ale_marega@hotmail.it>
2021-06-16 13:21:33 +0200

    fix sintassi

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -17,29 +17,7 @@ Booleat
 <div id="app">
     <h1 class="text-center">Benvenuto su Booleat</h1>
      <div class="container text-center">
-        {{-- <select v-model="search" v-on:change="filterGenre">
-            <option value="">All</option>
-            @foreach ($genres as $genre)
-                
-                <option value="{{ $genre->id }}">
-                    {{ $genre->name }}
-                </option>
-                
-            @endforeach
-        </select>
-        <ul  v-if= "search == '' ">
-            <li v-for="restaurant in restaurants">
-                @{{ restaurant.restaurant_name }}
-            </li>
-        </ul> 
-    
-         <ul v-else>
-            @foreach ($users as $user)
-                <li>
-                    {{ $user->restaurant_name }}
-                </li>
-            @endforeach
-        </ul> --}}
+  
         <input  type="text" placeholder="cerca categorie" value="" v-model="search">
         <button class="btn btn-success" type="button" name="button" v-on:click="filterGenre">Search</button>
          <div class="container" v-for="restaurant in restaurants">
