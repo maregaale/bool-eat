2e3737e8faa214468a41bf92f4a39266cf47df4a
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-06-16 13:15:17 +0200

    filter buttons top

--- public/js/guest-index.js
+++ public/js/guest-index.js
@@ -9,19 +9,20 @@ new Vue({
     search: '',
     restaurants: [],
     selectRestaurant: '',
-    mntRestaurant: []
+    genres: []
   },
   //Mounted
   mounted: function mounted() {
     var _this = this;
 
-    axios.get('http://localhost:8000/api/search').then(function (resp) {
-      _this.albums = resp.data;
+    axios.get('http://localhost:8000/api/genres').then(function (resp) {
+      _this.genres = resp.data;
+      console.log(_this.genres);
     });
   },
   //\Mounted
   methods: {
-    //Filtro per genres
+    //Filtro per genres!!!!!
     filterGenre: function filterGenre() {
       var _this2 = this;
 
@@ -32,7 +33,21 @@ new Vue({
         console.log(result.data);
         _this2.restaurants = result.data; //console.log(this.restaurants);
       });
-    }
+    },
+    //Fine Funzione Filtro per genres!!!!!!!!!!!!
+    //Filtro per categorie con bottoni
+    filterGenreButtons: function filterGenreButtons(value) {
+      var _this3 = this;
+
+      axios.get('http://localhost:8000/api/filterapi/' + value, {// params: {
+        //   search: this.search
+        // }
+      }).then(function (result) {
+        console.log(result.data);
+        _this3.restaurants = result.data; //console.log(this.restaurants);
+      });
+    } // Fine Filtro per categorie con bottoni
+
   }
 });
 /******/ })()
