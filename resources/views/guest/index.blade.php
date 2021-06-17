7a7ed8e7df5771f453d0ff1df8b808df4879d2fa
Giorgio Rindonone <giorgio.rindonone@gmail.com>
2021-06-17 10:53:24 +0200

    edit login page

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -14,15 +14,32 @@ Booleat
 
 
 @section('content')
+   
 <div id="app">
-    <h1 class="text-center">Benvenuto su Booleat</h1>
-     <div class="container text-center">
-  
+
+      <!-- Jumbotron -->
+      <div
+      class=" text-center "
+      style="      background-color: rgba(0, 0, 0, 0.6);;      height: 300px;">
+         
+            <div class="d-flex justify-content-center align-items-center ">
+               <div class="text-white mt-5">
+                  <h1 class="mb-3">Benvenuto su Booleat</h1>
+                  <h4 class="mb-3">cerca e ordina!</h4>
+                  
+                  <input  type="text" placeholder="cerca categorie" value="" v-model="search">
+                  <button class="btn btn-outline-light btn-success btn-lg" type="button" name="button" v-on:click="filterGenre">Search</button>
+               </div>
+            </div>
+         
+      </div>
+      <!-- Jumbotron -->   
+    {{-- <h1 class="text-center"></h1> --}}
+     {{-- <div class="container text-center btn-5">  
         <input  type="text" placeholder="cerca categorie" value="" v-model="search">
         <button class="btn btn-success" type="button" name="button" v-on:click="filterGenre">Search</button>
-    </div>
-
-        <nav class="nav_btn">
+    </div> --}}
+        <nav class="nav_btn mt-5">
            <div v-for="genre in genres">
             <button class="btn_genre" v-on:click="filterGenreButtons(genre.name)">@{{ genre.name }}</button>
            </div>
