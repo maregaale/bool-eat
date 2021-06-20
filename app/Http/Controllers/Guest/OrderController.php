1eebfa057bda767d376607c565f9cdb88b5d8774
Daniele <danielelai86@gmail.com>
2021-06-20 15:40:21 +0200

    Aggiunto test di pagamento

--- app/Http/Controllers/Guest/OrderController.php
+++ app/Http/Controllers/Guest/OrderController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Guest;
-
+use Braintree\Gateway as Gateway;
+use Braintree\Transaction as Transaction;
 use App\Http\Controllers\Controller;
 use App\Order;
 use App\User;
@@ -13,8 +14,49 @@ class OrderController extends Controller
     {
         $restaurant = User::find($id)->first();
 
-        return view('guest.checkout', compact('restaurant'));
+        $gateway = new Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => '6fgmgkg9h9rz85kp',
+            'publicKey' => 'xfg5vxxfb7ycbdtk',
+            'privateKey' => '9a6b94af4897873269d5453a18f6b5c3'
+          ]);
 
+          // pass $clientToken to your front-end
+        $clientToken = $gateway->clientToken()->generate([]);
+
+        // $nonceFromTheClient = $_POST["payment_method_nonce"];
+        /* Use payment method nonce here */
+
+        // $result = $gateway->transaction()->sale([
+        //     'amount' => '10.00',
+        //     'paymentMethodNonce' => $nonceFromTheClient,
+        //     'deviceData' => $deviceDataFromTheClient,
+        //     'options' => [
+        //       'submitForSettlement' => True
+        //     ]
+        //   ]);
+
+        // Result di prova
+        $result = $gateway->transaction()->sale([
+            'amount' => '10.00',
+            'paymentMethodNonce' => 'fake-valid-nonce',
+            'options' => [
+              'submitForSettlement' => True
+            ]
+          ]);
+          if ($result->success) {
+            print_r("success!: " . $result->transaction->id);
+        } else if ($result->transaction) {
+            print_r("Error processing transaction:");
+            print_r("\n  code: " . $result->transaction->processorResponseCode);
+            print_r("\n  text: " . $result->transaction->processorResponseText);
+        } else {
+            foreach($result->errors->deepAll() AS $error) {
+              print_r($error->code . ": " . $error->message . "\n");
+            }
+        }
+
+        return view('guest.checkout', compact('restaurant'));
 
     }
 }
--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -2,6 +2,7 @@
 
 
 @section('style')
+<script src="https://js.braintreegateway.com/web/dropin/1.30.1/js/dropin.min.js"></script>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
 @endsection
 
@@ -11,13 +12,11 @@ Booleat | Completa l'Ordine
     
 @endsection
 
-
 @section('content')
 <div class="container text-center">
+   
 
- <form id="pay_form" method="POST" enctype="multipart/form-data"> 
-    @csrf
-    @method('POST')
+  <form id="payment-form" action="/route/on/your/server" method="post"> 
     <div >
         <label for="name">Nome</label>
         <input type="text"  id="name" name="name"  placeholder="Inserisci il nome" required>
@@ -41,9 +40,12 @@ Booleat | Completa l'Ordine
 
     </div>
 
-    <button class="btn-success" type="submit" >Completa l' Ordine</button>
+    {{-- <button class="btn-success" type="submit" >Completa l' Ordine</button> --}}
 
-    
+     <!-- Step one: add an empty container to your page -->
+    <div id="dropin-container"></div>
+    <input type="submit" />
+    <input type="hidden" id="nonce" name="payment_method_nonce"/>
  </form>
 </div>
     
@@ -51,5 +53,36 @@ Booleat | Completa l'Ordine
 
 
 @section('script')
+<script type="text/javascript">
+    const form = document.getElementById('payment-form');
+    // call `braintree.dropin.create` code here
+    // Step two: create a dropin instance using that container (or a string
+    //   that functions as a query selector such as `#dropin-container`)
+    braintree.dropin.create({
+        // Step three: get client token from your server, such as via
+        //    templates or async http request
+        authorization: sandbox_4xdmykmg_6fgmgkg9h9rz85kp,
+        container: document.getElementById('dropin-container'),
+        // ...plus remaining configuration
+    }, (error, dropinInstance) => {
+        // Use `dropinInstance` here
+        if (error) console.error(error);
 
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+
+            dropinInstance.requestPaymentMethod((error, payload) => {
+                if (error) console.error(error);
+                // Step four: when the user is ready to complete their
+                //   transaction, use the dropinInstance to get a payment
+                //   method nonce for the user's selected payment method, then add
+                //   it a the hidden field before submitting the complete form to
+                //   a server-side integration
+                document.getElementById('nonce').value = payload.nonce;
+                form.submit();
+            });
+        });
+        // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
+    });
+</script>
 @endsection
