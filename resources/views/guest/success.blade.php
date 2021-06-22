518a1085397fef06dfc432f98802ccecb1f2a7a9
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-06-22 10:56:53 +0200

    fix stile success page

--- resources/views/guest/success.blade.php
+++ resources/views/guest/success.blade.php
@@ -12,6 +12,15 @@ Success!
 
 
 @section('content')
+
+<section id="success">
+    <div class="box_success">
+     <h1>Il tuo ordine è stato effettuato correttamente!!</h1>
+     <img src="{{ asset('images/delivery.jpg')}}" alt="delivery">
+     <p>Un fattorino ha preso i dati del tuo ordine è sta arrivando da te!</p>
+    </div>
+
+</section>
     
 @endsection
 
