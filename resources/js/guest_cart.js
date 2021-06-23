33206d6f3897e344a53a2a813011582797a410d4
Alessandro Marega <ale_marega@hotmail.it>
2021-06-23 09:23:40 +0200

    aggiunta funzionalità 'quantità' a carrello completa

--- resources/js/guest_cart.js
+++ resources/js/guest_cart.js
@@ -1,36 +1,64 @@
 new Vue({
   el: '#cart',
   data: {
+    namePlatesShow: [],
+    pricesShow: [],
     namePlates: [],
     prices: [],
     usersId: [],
     sum: 0,
     display: false,
     total: 0,
+    quantity: [],
   },
 
   //Methods
   methods:{
+    summatory: function () {
+
+      this.sum = 0;
+      this.prices = [];
+
+      
+
+      for (let i = 0; i < this.namePlates.length; i++) {
+
+
+        for (let k = 0; k < this.quantity[i] - 1; k++) {
+          
+          this.namePlates.push(this.namePlates[i]);
+
+          
+        }
+
+      }
+
+      for (let i = 0; i < this.namePlatesShow.length; i++) {
+        
+        
+        for (let k = 0; k < this.quantity[i]; k++) {
+          this.prices.push(this.pricesShow[i]);
+  
+          
+        }
+      }
+      
+      for (let i = 0; i < this.prices.length; i++) {
+        this.sum += this.prices[i];
+      }
+      
+    },
+
     
     addElementsToCart: function (name, price) {
       this.namePlates.push(name);
       this.prices.push(price);
     },
 
-    totalPrice: function (price) {
-
-      this.prices.push(price);
-
-      this.sum = 0;
-
-      for (let i = 0; i < this.prices.length; i++) {
-        this.sum += this.prices[i];
-      }
-
-      this.display = true;
-    }, 
-
+    
     removePrevCart: function (name, price, index) {
+      this.quantity.push(1);
+      
 
       this.usersId.push(index);
       
@@ -40,7 +68,11 @@ new Vue({
   
             this.usersId.push(index);
             this.namePlates = [];
+            this.namePlatesShow = [];
+
             this.prices = [];
+            this.pricesShow = [];
+
          
           
         } else {
@@ -50,31 +82,83 @@ new Vue({
       };
 
       this.namePlates.push(name);
+      if (!this.namePlatesShow.includes(name)) {
+        this.namePlatesShow.push(name);
+      }
+
     },
+    totalPrice: function (price) {
+      if (!this.pricesShow.includes(price)) {
+        this.pricesShow.push(price);
+      }
+      
+      this.prices.push(price);
+
+      this.sum = 0;
+
+
+      for (let i = 0; i < this.pricesShow.length; i++) {
+
+
+        for (let k = 0; k < this.quantity[i]; k++) {
+          
+          this.sum = this.sum + this.pricesShow[i];
+
+          
+        }
+
+      }
+
+
+      
+
+      this.display = true;
+    }, 
+
 
     removeCartElement: function (index) {
       
       this.namePlates.splice(index, 1);
-        
+      this.namePlatesShow.splice(index, 1);
+      this.quantity.splice(index, 1);
     },
 
     removePrice: function (index, price) {
       this.prices.splice(index, 1);
+      this.pricesShow.splice(index, 1);
 
-      this.sum = this.sum - price;
+      // for (let i = 0; i < this.prices.length; i++) {
+      //   this.sum - this.prices[i];
+      // }
+      this.sum = 0;
+      for (let i = 0; i < this.pricesShow.length; i++) {
+          this.sum += this.quantity[i] * this.pricesShow[i];
+      }
+
+      // this.sum = this.sum - price;
     },
-
   },
+
   
   mounted() {
     this.usersId = []
-    
+    this.quantity = JSON.parse(localStorage.getItem("quantity")) || [],
     this.namePlates = JSON.parse(localStorage.getItem("namePlates")) || [],
-    this.prices = JSON.parse(localStorage.getItem("prices")) || []
+    this.namePlatesShow = JSON.parse(localStorage.getItem("namePlatesShow")) || [],
+    this.prices = JSON.parse(localStorage.getItem("prices")) || [],
+    this.pricesShow = JSON.parse(localStorage.getItem("pricesShow")) || [],
     this.usersId = JSON.parse(localStorage.getItem("usersId")) || []
 
-    for (let i = 0; i < this.prices.length; i++) {
-      this.sum += this.prices[i];
+    for (let i = 0; i < this.pricesShow.length; i++) {
+
+
+      for (let k = 0; k < this.quantity[i]; k++) {
+        
+        this.sum = this.sum + this.pricesShow[i];
+
+        
+      }
+
     }
   },
   
@@ -82,31 +166,24 @@ new Vue({
     namePlates(newValue, oldValue) {
       localStorage.setItem("namePlates", JSON.stringify(newValue));
     },
+    namePlatesShow(newValue, oldValue) {
+      localStorage.setItem("namePlatesShow", JSON.stringify(newValue));
+    },
     prices(newValue, oldValue) {
       localStorage.setItem("prices", JSON.stringify(newValue));
+    },
+    pricesShow(newValue, oldValue) {
+      localStorage.setItem("pricesShow", JSON.stringify(newValue));
     },
     usersId(newValue, oldValue) {
       localStorage.setItem("usersId", JSON.stringify(newValue));
     },
     sum(newValue, oldValue) {
       localStorage.setItem("sum", JSON.stringify(newValue));
-    }
-  }  
-  
-    
-   
-  
-
-    
-
-
-
-  
-  
-
-
-
-
-
+    },
+    quantity(newValue, oldValue) {
+      localStorage.setItem("quantity", JSON.stringify(newValue));
+    },
+  }
 
 });
--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -30,7 +30,7 @@
             <p>{{ $plate->ingredients }}</p>
             <h4>{{$plate->price}} &euro;</h4>
             <span><i class="fas fa-cart-arrow-down"></i> Aggiungi al carrello</span>
-            <button v-on:click="removePrevCart({{ json_encode($plate->name) }}, {{ json_encode($plate->price) }}, {{ json_encode($plate->user_id) }}); totalPrice({{ json_encode($plate->price) }})" class="btn btn-success"><i class="fas fa-plus"></i></button>
+            <button v-on:click.once="removePrevCart({{ json_encode($plate->name) }}, {{ json_encode($plate->price) }}, {{ json_encode($plate->user_id) }}); totalPrice({{ json_encode($plate->price) }})" class="btn btn-success"><i class="fas fa-plus"></i></button>
             <p class="font-weight-bold">{{$plate->price}} &euro;</p>
           </div>
           <div class="img-plate">
@@ -50,23 +50,28 @@
       <h3 class="text-right">Carrello</h3>
       <hr>
       <div class="elements_container">
+        <div>
+          
+          <input v-on:change="summatory()" v-for="(namePlate, index) in namePlatesShow" type="number" min=1 placeholder="quantity" v-model.number="quantity[index]" >
+
+        </div>
         <div class="name">
-          <p v-for="namePlate in namePlates">@{{namePlate}}</p>
+          <p v-for="namePlate in namePlatesShow">@{{namePlate}}</p>
         </div>
         <div class="price">
-          <p v-for="price in prices">@{{price}} &euro;</p>
+          <p v-for="price in pricesShow">@{{price}} &euro;</p>
         </div>
         <div class="button">
-          <button v-on:click="removeCartElement(index); removePrice(index, price)" v-for="(price, index) in prices" v-if="namePlates.length != 0" class="btn btn-danger">Elimina</button>
+          <button v-on:click="removeCartElement(index); removePrice(index, price)" v-for="(price, index) in pricesShow" v-if="namePlates.length != 0" class="btn btn-danger">Elimina</button>
         </div>        
       </div>
 
       <div class="sum mt-4 mb-4">
-        <h3 v-if="namePlates.length != 0" >Totale: @{{sum}} &euro;</h3>
+        <h3 v-if="namePlatesShow.length != 0" >Totale: @{{sum}} &euro;</h3>
       </div>
 
-      <div class="checkout" v-if="namePlates.length != 0" class="submit mt-3">
-        <button v-if class="btn"><a href="{{ route('guest.checkout' , $user->id)}}">Completa l'ordine</a></button>
+      <div class="checkout" v-if="namePlatesShow.length != 0" class="submit mt-3">
+        <button v-on:click="addStorage()" v-if class="btn"><a href="{{ route('guest.checkout' , $user->id)}}">Completa l'ordine</a></button>
       </div>
     
     </div>
