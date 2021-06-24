5fcee9626618fa4a5eadf8514dfd76501a4a3b49
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-06-24 16:44:50 +0200

    api che filtra gli ordini per ogni id dell'admin

--- resources/views/prova_chartjs.blade.php
+++ resources/views/prova_chartjs.blade.php
@@ -12,54 +12,57 @@
     <title>Document</title>
   </head>
   <body>
-    <div>
       
       <div class="container chart_container" style="width: 400px">
+      <div class="" id="stats">
 
         <canvas id="myChart" width="400" height="400"></canvas>
-          <script>
-            var ctx = document.getElementById('myChart');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    
-                    labels: [@foreach($orders as $order) "{{$order->name}}", @endforeach ],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [@foreach($orders as $order) "{{$order->total}}", @endforeach ],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: true,
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-          </script>
+      </div>
+         
         
       </div>
-    </div>
+
+
+    <script>
+      var ctx = document.getElementById('myChart');
+      var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+              
+              labels: [@foreach($orders as $order) "{{$order->name}}", @endforeach ],
+              datasets: [{
+                  label: '# of Votes',
+                  data: [@foreach($orders as $order) "{{$order->total}}", @endforeach ],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              responsive: true,
+              maintainAspectRatio: true,
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
+    </script>
     
   </body>
 </html>
