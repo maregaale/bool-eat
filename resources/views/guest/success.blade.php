e900814b6bba7158adb08c76857c525b60c57fb5
Giorgio Rindonone <giorgio.rindonone@gmail.com>
2021-06-27 16:56:16 +0200

    style_order_complete

--- resources/views/guest/success.blade.php
+++ resources/views/guest/success.blade.php
@@ -10,19 +10,49 @@
     
 @endsection
 
-@include('partials.header')
 
 
 @section('content')
 
-<section id="success">
-    <div class="box_success">
-     <h1>Il tuo ordine è stato effettuato correttamente!!</h1>
-     <img src="{{ asset('images/delivery.jpg')}}" alt="delivery">
-     <p>Un fattorino ha preso i dati del tuo ordine è sta arrivando da te!</p>
+
+<section class="  py-md-0 back-checkout">
+    <div class="back-checkout-container">
+        <div class=" container d-flex flex-column align-items-center mb-5 ">
+            <div class="card register-card   mb-5 ">
+                <div class="row  no-gutters ">
+                    <div class="card-body d-flex flex-column align-items-center">
+    
+    
+                        <div>
+                            <a class="homebutton anchor-link" href="{{ url('/') }}">Torna alla home</a>
+    
+    
+                            <h2 class="mt-1 mb-4 font-weight-bold">Il tuo ordine è stato effettuato correttamente!!</h2>
+    
+                            {{-- <p class="login-card-description">Inserisci ora i tuoi dati per completare l'ordine</p> --}}
+                            <p class="col-form-label ">Un fattorino ha preso i dati del tuo ordine è sta arrivando da te!</p>
+
+        
+        
+                            
+    
+                        </div>
+    
+                        <img class="image-fluid" src="{{asset('images/order-success.jpg')}}" alt="">
+    
+                    </div>
+                </div>
+            </div>
+    
+    
+        </div>
+
     </div>
 
+
+
 </section>
+
     
 @endsection
 
