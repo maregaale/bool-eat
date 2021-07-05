7b3eeb88bf4fbef54196fccafc4c72b2d529823e
Alessandro Marega <ale_marega@hotmail.it>
2021-07-05 13:34:27 +0200

    refactoring index.blade

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -13,49 +13,37 @@
 
   @include('partials.header')
   <div id="app"> 
-  <!-- Jumbotron -->
-  <div class="mb-3 jumbotron_wrapper">
-    <div class="text-center height-text-jumbotron">
-      <div class="d-flex justify-content-center align-items-center "> 
-        {{-- jumbotron_content --}}
-        <div class="jumbotron_content text-white">
-             <img src="" alt="">
-            <h1 class="mb-3">Benvenuto su Booleat</h1>
-            <div class="title">
-              {{-- <h1>Benvenuto<br/>su Booleat</h1> --}}
-             </div>
-            <h2 class="mb-3">Affidati al top del top, scopri i nostri ristoranti!</h2>
+    <!-- Jumbotron -->
+    <div class="mb-3 jumbotron_wrapper">
+      <div class="text-center height-text-jumbotron">
+        <div class="d-flex justify-content-center align-items-center "> 
+          {{-- jumbotron_content --}}
+          <div class="jumbotron_content text-white">
+              <img src="" alt="">
+              <h1 class="mb-3">Benvenuto su Booleat</h1>
+              <div class="title">
+                {{-- <h1>Benvenuto<br/>su Booleat</h1> --}}
+              </div>
+              <h2 class="mb-3">Affidati al top del top, scopri i nostri ristoranti!</h2>
+          </div>
+        </div>
+      </div>
+      <div class="text-center jumbotron_bottom">
+        <div class="d-flex justify-content-center align-items-center ">
+          <div class="text-white">
+              <h2 class="mb-4">Cerca e ordina!</h2>
+              {{-- Search names --}}
+              <input  type="text" placeholder="Nome ristorante" value="" v-model="restaurantName"  v-on:keydown="searchName">
+              <button type="button" class="button_card_menu " name="button" v-on:click="searchName"><i class="fas fa-search"></i></button>
+              {{-- Search names --}}
+          </div>
         </div>
       </div>
     </div>
-    <div class="text-center jumbotron_bottom">
-      <div class="d-flex justify-content-center align-items-center ">
-        <div class="text-white">
-            <h2 class="mb-4">Cerca e ordina!</h2>
-            {{-- Search names --}}
-            <input  type="text" placeholder="Nome ristorante" value="" v-model="restaurantName"  v-on:keydown="searchName">
-            <button type="button" class="button_card_menu " name="button" v-on:click="searchName"><i class="fas fa-search"></i></button>
-            {{-- Search names --}}
-        </div>
-      </div>
-    </div>
-  </div>
+    <!-- /Jumbotron -->
 
-
-
-
-  <!-- Jumbotron search -->
-   
-
-    {{-- <button v-on:click="darkThemeSwitch">
-      
-       Dark
-    </button>  --}}
-
- 
-
+    {{-- generi --}}
     <div class="wrapper_genres">
-      {{-- <h2>...puoi sempre cercare per genere. Facile no?</h2> --}}
       <div id="navbarNav" class="navbar_genres">
         <div class="navbar_genres-cont v-dragscroll.y">
           <div class="active genres_content text-center" v-for="genre in genres">
@@ -67,7 +55,9 @@
         </div>
       </div>
     </div>
-  
+    {{-- /generi --}}
+
+    {{-- ristoranti --}}
     <div class="container_card">
       <div class="card_restaurant" v-for="user in users">
           <img v-bind:src="user.logo" alt="">
@@ -77,53 +67,50 @@
           <button class="btn_menu_overlay button_card_menu"><a :href="'http://localhost:8000/show/'+ user.id">Visualizza menù</a></button>
         </div>
       </div>
-    {{-- /card Ricerca Nome --}}
+      {{-- /card Ricerca Nome --}}
 
-    {{-- Card ricerca categorie --}}
+      {{-- Card ricerca categorie --}}
       <div class="card_restaurant" v-for="restaurant in restaurants">
         <img v-bind:src="restaurant.logo" alt="">
         <h4>@{{restaurant.restaurant_name}}</h4>
-      <div class="overlay">
-        <p class="text card_address mt-2">@{{restaurant.address}}</p>
-        <button class="btn_menu_overlay button_card_menu"><a :href="'http://localhost:8000/show/'+ restaurant.id">Visualizza menù</a></button>
+        <div class="overlay">
+          <p class="text card_address mt-2">@{{restaurant.address}}</p>
+          <button class="btn_menu_overlay button_card_menu"><a :href="'http://localhost:8000/show/'+ restaurant.id">Visualizza menù</a></button>
+        </div>
+      </div>
+      {{-- /Card ricerca categorie --}}
+    </div>
+    {{-- /ristoranti --}}
+
+    {{-- ristoranti --}}
+    <div class="card_container_2">
+      <div class="div text-center">
+        <h2>I Nostri migliori Ristoranti</h2>
+      </div>
+
+      <div class="container_card">
+        {{-- Tutti i ristoranti --}}
+          @foreach ($users as $user)
+              <div class="card_restaurant"> 
+                <img src="{{$user->logo}}" alt="">
+                <h4>{{$user->restaurant_name}}</h4> 
+                <div class="overlay">
+                  <p class="text card_address mt-2">{{$user->address}}</p>
+                  <a v-on:click="message({{ json_encode($user->id) }})" href="{{ route('guest.show' , ['user' => $user->id])}}"><button class="btn_menu_overlay button_card_menu">Vai al Menù</button></a>
+                </div>
+            </div>
+          @endforeach
+        {{-- /Tutti i ristoranti --}}
       </div>
     </div>
-    {{-- /Card ricerca categorie --}}
-   </div>
-
-  <div class="card_container_2">
-    <div class="div text-center">
-      {{-- <h1><span>S</span><span>c</span><span>o</span><span>p</span><span>r</span><span>i</span> <span>i</span> <span>n</span><span>o</span><span>s</span><span>t</span><span>r</span><span>i</span> <span>r</span><span>i</span><span>s</span><span>t</span><span>o</span><span>r</span><span>a</span><span>n</span><span>t</span><span>i</span></h1> --}}
-      <h2>I Nostri migliori Ristoranti</h2>
-    </div>
-
-    <div class="container_card">
-      {{-- Tutti i ristoranti --}}
-        @foreach ($users as $user)
-            <div class="card_restaurant"> 
-              <img src="{{$user->logo}}" alt="">
-              <h4>{{$user->restaurant_name}}</h4> 
-              <div class="overlay">
-                <p class="text card_address mt-2">{{$user->address}}</p>
-                <a v-on:click="message({{ json_encode($user->id) }})" href="{{ route('guest.show' , ['user' => $user->id])}}"><button class="btn_menu_overlay button_card_menu">Vai al Menù</button></a>
-              </div>
-          </div>
-        @endforeach
-      {{-- /Tutti i ristoranti --}}
-    </div>
-  </div>
-
+    {{-- /ristoranti --}}
   </div>   
-
 
 @include('partials.footer')
 
 @endsection
 
 @section('script')
-
-  
   <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-
   <script src="{{ asset('js/guest-index.js')}}"></script>
  @endsection
